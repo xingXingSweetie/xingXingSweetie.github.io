08fc7f433114b5a4d161f97491c95021528cad02
xingXingSweetie <75576411+xingXingSweetie@users.noreply.github.com>
2021-11-28 17:40:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../public/index.html'),
     assetsRoot: path.resolve(__dirname, '../public'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/xingXingSweetie.github.io/public/',
+    assetsPublicPath: '/xingXingSweetie.github.io/dist',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
